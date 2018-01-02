01da77de264f7ad02b44fc988c412176f0d970b7
luv39 <1519667170@qq.com>
2018-01-02 21:01:44 +0800

    refresh

--- pyepub.py
+++ pyepub.py
@@ -47,7 +47,6 @@ def findStoryName(text):
     if len(storyname) == 0:
         print '没有找到小说名,请确认输入了正确的URL fail'
         return 404
-    print storyname[0] + ' start download'
     return storyname[0]
 
 def findHomePage(storyname, text):
@@ -56,7 +55,7 @@ def findHomePage(storyname, text):
     except:
         return 404
     r = requests.get(homepageurl)
-    f = open('%s/OEBPS/cover.jpg'%storyname, 'wb')
+    f = open('''%s/OEBPS/cover.jpg'''%storyname, 'wb')
     f.write(r.content)
     f.close()
 
@@ -103,16 +102,16 @@ def init(storyname):
     isExists = os.path.exists(storyname)
 
     if not isExists:
-        os.makedirs('%s/META-INF'%storyname)
-        os.makedirs('%s/OEBPS'%storyname)
+        os.makedirs('''%s/META-INF'''%storyname)
+        os.makedirs('''%s/OEBPS'''%storyname)
         print storyname + ' creat success'
     else:
         print storyname + ' is exists'
         return False
 
-    shutil.copy('mimetype', '%s'%storyname)
-    shutil.copy('container.xml', '%s/META-INF'%storyname)
-    shutil.copy('stylesheet.css', '%s/OEBPS'%storyname)
+    shutil.copy('mimetype', '''%s'''%storyname)
+    shutil.copy('container.xml', '''%s/META-INF'''%storyname)
+    shutil.copy('stylesheet.css', '''%s/OEBPS'''%storyname)
 
 def writeFile(filename, line):
     f = open(filename, 'a')
@@ -124,84 +123,89 @@ def removeFile(filename):
         os.remove(filename)
 
 def creatMetadata(storyname):
-    writeFile('%s/metadata' % storyname, '''<?xml version='1.0' encoding='utf-8'?>''')
-    writeFile('%s/metadata' % storyname, '''<package xmlns="http://www.idpf.org/2007/opf" xmlns:dc="http://purl.org/dc/elements/1.1/" unique-identifier="bookid" version="2.0">''')
-    writeFile('%s/metadata' % storyname, '''<metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">''')
-    writeFile('%s/metadata' % storyname, '<dc:title>' + storyname + '</dc:title>')
-    writeFile('%s/metadata' % storyname, '<dc:language>zh-cn</dc:language>')
-    writeFile('%s/metadata' % storyname, '</metadata>')
+    writeFile('''%s/metadata''' % storyname, '''<?xml version='1.0' encoding='utf-8'?>''')
+    writeFile('''%s/metadata''' % storyname, '''<package xmlns="http://www.idpf.org/2007/opf" xmlns:dc="http://purl.org/dc/elements/1.1/" unique-identifier="bookid" version="2.0">''')
+    writeFile('''%s/metadata''' % storyname, '''<metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">''')
+    writeFile('''%s/metadata''' % storyname, '<dc:title>' + storyname + '</dc:title>')
+    writeFile('''%s/metadata''' % storyname, '<dc:language>zh-cn</dc:language>')
+    writeFile('''%s/metadata''' % storyname, '</metadata>')
 
 def creatManifestAndSpineAndNavmap(href, id, chapter, title, storyname):
-    writeFile('%s/manifest' % storyname, '<item href="' + href + '" id="' + id + '" media-type="application/xhtml+xml"/>' )
-    writeFile('%s/spine' % storyname, '<itemref idref="' + id + '"/>')
-    writeFile('%s/navmap' % storyname, '<navPoint id="%s" playOrder="%d"><navLabel><text>%s</text></navLabel><content src="%s"/></navPoint>' % (id, chapter, title, href))
+    writeFile('''%s/manifest''' % storyname, '<item href="' + href + '" id="' + id + '" media-type="application/xhtml+xml"/>' )
+    writeFile('''%s/spine''' % storyname, '<itemref idref="' + id + '"/>')
+    writeFile('''%s/navmap''' % storyname, '''<navPoint id="%s" playOrder="%d"><navLabel><text>%s</text></navLabel><content src="%s"/></navPoint>''' % (id, chapter, title, href))
 
 def creatContent(storyname):
-    f = open('%s/metadata' % storyname,'r')
+    f = open('''%s/metadata''' % storyname,'r')
     metadata = f.readlines()
     f.close()
     for line in metadata:
-        writeFile('%s/OEBPS/content.opf'%storyname, line)
+        writeFile('''%s/OEBPS/content.opf'''%storyname, line)
 
-    writeFile('%s/OEBPS/content.opf'%storyname, '<manifest>')
-    f = open('%s/manifest' % storyname, 'r')
+    writeFile('''%s/OEBPS/content.opf'''%storyname, '<manifest>')
+    f = open('''%s/manifest''' % storyname, 'r')
     manifest = f.readlines()
     f.close()
     for line in manifest:
-        writeFile('%s/OEBPS/content.opf'%storyname, line)
-    writeFile('%s/OEBPS/content.opf'%storyname, '<item href="stylesheet.css" id="css" media-type="text/css"/>')
-    writeFile('%s/OEBPS/content.opf'%storyname, '<item href="toc.ncx" media-type="application/x-dtbncx+xml" id="ncx"/>')
-    writeFile('%s/OEBPS/content.opf'%storyname, '<item href="cover.jpg" id="cover" media-type="jpeg"/>')
-    writeFile('%s/OEBPS/content.opf'%storyname, '</manifest>')
+        writeFile('''%s/OEBPS/content.opf'''%storyname, line)
+    writeFile('''%s/OEBPS/content.opf'''%storyname, '<item href="stylesheet.css" id="css" media-type="text/css"/>')
+    writeFile('''%s/OEBPS/content.opf'''%storyname, '<item href="toc.ncx" media-type="application/x-dtbncx+xml" id="ncx"/>')
+    writeFile('''%s/OEBPS/content.opf'''%storyname, '<item href="cover.jpg" id="cover" media-type="jpeg"/>')
+    writeFile('''%s/OEBPS/content.opf'''%storyname, '</manifest>')
 
-    writeFile('%s/OEBPS/content.opf'%storyname, '<spine toc="ncx">')
-    f = open('%s/spine' % storyname, 'r')
+    writeFile('''%s/OEBPS/content.opf'''%storyname, '<spine toc="ncx">')
+    f = open('''%s/spine''' % storyname, 'r')
     spine = f.readlines()
     for line in spine:
-        writeFile('%s/OEBPS/content.opf' % storyname, line)
-    writeFile('%s/OEBPS/content.opf'%storyname, '</spine><guide><reference href="cover.jpg" type="cover" title="封面"/></guide></package>')
+        writeFile('''%s/OEBPS/content.opf''' % storyname, line)
+    writeFile('''%s/OEBPS/content.opf'''%storyname, '</spine><guide><reference href="cover.jpg" type="cover" title="封面"/></guide></package>')
 
-    removeFile('%s/manifest' % storyname)
-    removeFile('%s/metadata' % storyname)
-    removeFile('%s/spine' % storyname)
+    removeFile('''%s/manifest''' % storyname)
+    removeFile('''%s/metadata''' % storyname)
+    removeFile('''%s/spine''' % storyname)
 
 def creatToc(storyname):
-    writeFile('%s/OEBPS/toc.ncx' % storyname, "<?xml version='1.0' encoding='utf-8'?>")
-    writeFile('%s/OEBPS/toc.ncx' % storyname, '<ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">')
-    writeFile('%s/OEBPS/toc.ncx' % storyname, '<docTitle><text>%s</text></docTitle>' % storyname)
-    writeFile('%s/OEBPS/toc.ncx' % storyname, '<navMap>')
-    f = open('%s/navmap' % storyname,'r')
+    writeFile('''%s/OEBPS/toc.ncx''' % storyname, "<?xml version='1.0' encoding='utf-8'?>")
+    writeFile('''%s/OEBPS/toc.ncx''' % storyname, '<ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">')
+    writeFile('''%s/OEBPS/toc.ncx''' % storyname, '''<docTitle><text>%s</text></docTitle>''' % storyname)
+    writeFile('''%s/OEBPS/toc.ncx''' % storyname, '<navMap>')
+    f = open('''%s/navmap''' % storyname,'r')
     navmap = f.readlines()
     f.close()
     for line in navmap:
-        writeFile('%s/OEBPS/toc.ncx' % storyname, line)
-    writeFile('%s/OEBPS/toc.ncx' % storyname, '</navMap></ncx>')
+        writeFile('''%s/OEBPS/toc.ncx''' % storyname, line)
+    writeFile('''%s/OEBPS/toc.ncx''' % storyname, '</navMap></ncx>')
 
-    removeFile('%s/navmap' % storyname)
+    removeFile('''%s/navmap''' % storyname)
 
 def creatChapter(storyname, href, title, story):
-    writeFile('%s/OEBPS/%s'%(storyname, href), '''<?xml version="1.0" encoding="utf-8" standalone="no"?>''')
-    writeFile('%s/OEBPS/%s' % (storyname, href), '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">''')
-    writeFile('%s/OEBPS/%s' % (storyname, href), '''<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN">''')
-    writeFile('%s/OEBPS/%s' % (storyname, href), '''<head>''')
-    writeFile('%s/OEBPS/%s' % (storyname, href), '''<title>%s</title>'''%title)
-    writeFile('%s/OEBPS/%s' % (storyname, href), '''<link href="stylesheet.css" type="text/css" rel="stylesheet"/><style type="text/css">''')
-    writeFile('%s/OEBPS/%s' % (storyname, href), '''@page { margin-bottom: 5.000000pt; margin-top: 5.000000pt; }</style>''')
-    writeFile('%s/OEBPS/%s' % (storyname, href), '''</head>''')
-    writeFile('%s/OEBPS/%s' % (storyname, href), '''<body>''')
-    writeFile('%s/OEBPS/%s' % (storyname, href), '''<h2><span style="border-bottom:1px solid">%s</span></h2>'''%title)
+    writeFile('''%s/OEBPS/%s'''%(storyname, href), '''<?xml version="1.0" encoding="utf-8" standalone="no"?>''')
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">''')
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '''<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN">''')
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '''<head>''')
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '''<title>%s</title>'''%title)
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '''<link href="stylesheet.css" type="text/css" rel="stylesheet"/><style type="text/css">''')
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '''@page { margin-bottom: 5.000000pt; margin-top: 5.000000pt; }</style>''')
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '''</head>''')
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '''<body>''')
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '''<h2><span style="border-bottom:1px solid">%s</span></h2>'''%title)
     for line in story:
-        writeFile('%s/OEBPS/%s' % (storyname, href), '<p>%s</p>'%line)
-    writeFile('%s/OEBPS/%s' % (storyname, href), '<div class="mbppagebreak"></div></body></html>')
+        writeFile('''%s/OEBPS/%s''' % (storyname, href), '''<p>%s</p>'''%line)
+    writeFile('''%s/OEBPS/%s''' % (storyname, href), '<div class="mbppagebreak"></div></body></html>')
 
 
 def storyDownLoad(url):
     main_text = getText(url)
 
     storyname = findStoryName(main_text)
+    replace_symbols = ('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ' ', '/', '.', '>', '<', ';')
+    for replace_symbol in replace_symbols:
+        storyname = storyname.replace(replace_symbol, '_')
+
     if storyname == 404:
         print 'task ' + url + ' download fail'
         return 404
+    print storyname + ' start download'
     init(storyname)
     creatMetadata(storyname)
     homepage = findHomePage(storyname, main_text)
@@ -232,7 +236,7 @@ def storyDownLoad(url):
             print mulu_url + " download fail"
             continue
 
-        id = 'chapter' + '%d'%chapter
+        id = 'chapter' + '''%d'''%chapter
         href = id + '.xhtml'
 
         creatManifestAndSpineAndNavmap(href, id, chapter, title, storyname)
@@ -245,13 +249,21 @@ def storyDownLoad(url):
     creatContent(storyname)
     creatToc(storyname)
     print 'epub make success'
-    os.chdir('./%s' % storyname)
-    os.system('zip -r %s.epub *' % storyname)
-    os.system('mv %s.epub ~/Documents' % storyname)
-    os.chdir('..')
-
+    #os.chdir('''/home/yaozhijin/PycharmProjects/storydl/%s''' % storyname)
+    #os.system('''zip -r %s.epub *''' % storyname)
+    #os.system('''mv %s.epub /media/yaozhijin/A304F9EFC859408D/biquge_story''' % storyname)
+    #os.chdir('/home/yaozhijin/PycharmProjects/storydl')
+    #os.system('''mv %s /media/yaozhijin/A304F9EFC859408D/biquge_story''' % storyname)
 
 if __name__ == '__main__':
-    url = 'http://www.biquge5200.com/79_79883/'
+    f = open('storydl.config', 'r')
+    urls = f.readlines()
+    f.close()
+    note = 1
 
-    storyDownLoad(url)
+    for url in urls:
+        print '''>-----------------------%d-------------------------------<''' % note
+        mark = storyDownLoad(url)
+        note += 1
+        if mark == 404:
+            continue
